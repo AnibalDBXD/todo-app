fd646a7b526746a8b704ad6703549c8842593085
AnibalDBXD <olivaresluisanibal@gmail.com>
2020-12-23 14:06:23 -0400

    [UPDATE] ALT Text, Delete.tsx

--- components/icons/Delete.tsx
+++ components/icons/Delete.tsx
@@ -1,5 +1,5 @@
 const Delete = () => (
-    <img src="https://img.icons8.com/flat_round/64/000000/delete-sign.png" />
+    <img src="https://img.icons8.com/flat_round/64/000000/delete-sign.png" alt="DELETE" />
 );
 
 export default Delete;
