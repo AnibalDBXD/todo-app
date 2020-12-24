fcc5a181bec8ea18a87ce55ccdba36af71ce9b1d
AnibalDBXD <olivaresluisanibal@gmail.com>
2020-12-23 20:16:07 -0400

    [UPDATE] Fix the Colors, tailwind.config.js :p

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './utils/**/*.{js,ts,jsx,tsx}'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
