4d551545f6620c233927f1bc2e424d2882964e0d
AnibalDBXD <olivaresluisanibal@gmail.com>
2020-12-24 17:11:17 -0400

    [ADD] Delete ALL button, CompletedTaskList.tsx

--- components/CompletedTaskList.tsx
+++ components/CompletedTaskList.tsx
@@ -1,15 +1,34 @@
-import { Task } from "../interfaces"
-import TaskList from "./TaskList"
+import { Task } from "../interfaces";
+import axios from "axios";
+import TaskList from "./TaskList";
+
+import config from "../config.json";
+import { useState } from "react";
 
 type Props = {
-    Tasks: Task[]
-}
+    Tasks: Task[],
+};
 
-const CompletedTaskList = ({ Tasks }: Props) => (
-    <div>
-        <h1 className="font-medium text-center text-xl m-2">Completed Task</h1>
-        <TaskList Tasks={Tasks} />
-    </div>
-)
+const CompletedTaskList = ({ Tasks }: Props) => {
+    const [CompletedTasks, setCompletedTasks] = useState(Tasks);
 
-export default CompletedTaskList
+    const deleteAll = () => {
+        Tasks.forEach((Task) => {
+            axios.delete(`${config.URL}/${Task._id}`);
+        });
+        setCompletedTasks([]);
+        alert("Success");
+    };
+
+    return (
+        <>
+            <h1 className="font-medium text-center text-xl m-2">Completed Task</h1>
+            <div className="flex justify-center">
+                <button className="bg-red-500 rounded-md shadow-sm p-1 px-2 text-white" onClick={deleteAll}>Delete All</button>
+            </div>
+            <TaskList Tasks={CompletedTasks} />
+        </>
+    );
+};
+
+export default CompletedTaskList;
